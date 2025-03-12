5e437619c038d77b55547f47f2ee12cf037e9066
Georgi Zahariev <74987305+Georgi-Zahariev@users.noreply.github.com>
2025-03-12 08:18:36 -0400

    Create MergeSortedArray.java

---
+++ Easy/MergeSortedArray.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] data = new int[m+n];
+        int index1=0;
+        int index2=0;
+
+        for(int i = 0 ; i<m+n ; i++){
+            if(index1<m && index2<n){
+                if(nums1[index1]<=nums2[index2]){
+                    data[i]=nums1[index1];
+                    index1++;
+                }else{
+                    data[i]=nums2[index2];
+                    index2++;
+                }
+            }else if(index1<m && index2>=n){
+                data[i]=nums1[index1];
+                index1++;
+            }else{
+                data[i]=nums2[index2];
+                index2++;
+            }
+        }
+
+        for(int i = 0 ; i<m+n ; i++){
+            nums1[i]=data[i];
+        }
+    }
+}
